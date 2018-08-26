c5c20e9ebd898454a57463222f83a0b571878f8c
Ben Gotthold <bengotthold@gmail.com>
2018-08-26 14:47:03 -0600

    falcon cors

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import time
-
 from flask import Flask
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return "Hello, cross-origin-world!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True,port=5000)
